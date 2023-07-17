56b57a5a158cf363b0cc94a2509c9bd0371f7d12
SlavikLiabin <122288413+SlavikLiabin@users.noreply.github.com>
2023-07-17 19:22:53 +0500

    Delete symbolSVG.js

--- symbolSVG.js
+++
@@ -1,8 +0,0 @@
-<svg display="none" xmlns="http://www.w3.org/2000/svg">
-  <symbol id="basketball" viewBox=" 0 0 128 128">
-    <g>*Здесь располагаем код иконки мяча*</g>
-  </symbol>
-  <symbol id="roller" viewBox="0 0 512 512">
-    <g>*Здесь располагаем код иконки роликов*</g>
-  </symbol>
-</svg>;
